670181a1a56ed4b70a4db69c45a8e827d8c3ef5b
irishmaestro <70972101+IrishMaestro@users.noreply.github.com>
2022-09-01 15:36:56 -0500

    .

--- src/main.rs
+++ src/main.rs
@@ -10,18 +10,47 @@ use sha256::digest_bytes;
 
 mod init;
 
-
-
 fn md5(p: &str) -> Digest {
     let b = p.as_bytes();
     let digest = compute(b);
     digest
 }
 
+fn md5_search(data: &String, reader: BufReader<File>) {
+    init::print_seq("🦝 Parsing md5 hash");
+    let input = Digest(vec_to_array(string_to_decimal(data)));
+    let mut hashes: HashMap<Digest, String> = HashMap::new();
+    init::print_seq("🦝 Hashing wordlist with md5 algorithm");
+    for line in reader.lines() {
+        let hash = md5(&line.as_ref().unwrap());
+        hashes.insert(hash, line.unwrap());
+    }
+    init::print_seq("🦝 Searching HashMap for password...");
+    match hashes.get(&input) {
+        Some(value) => println!("🦝 Password found: <{}>", value.green().bold()),
+        None => println!("Password not found...")
+    }
+}
+
 fn sha256(p: &str) -> String {
     let b = p.as_bytes();
     let digest = digest_bytes(b);
     digest
+}
+
+fn sha256_search(input: &String, reader: BufReader<File>) {
+    init::print_seq("🦝 Parsing sha256 hash");
+    let mut hashes: HashMap<String, String> = HashMap::new();
+    init::print_seq("🦝 Hashing wordlist with sha256 algorithm");
+    for line in reader.lines() {
+        let hash = sha256(&line.as_ref().unwrap());
+        hashes.insert(hash, line.unwrap());
+    }
+    init::print_seq("🦝 Searching HashMap for password...");
+    match hashes.get(input) {
+        Some(value) => println!("🦝 Password found: <{}>", value.green().bold()),
+        None => println!("Password not found...")
+    }
 }
 
 // fn decimal_to_string(d: Digest) -> String {
@@ -52,27 +81,15 @@ fn vec_to_array<T>(v: Vec<T>) -> [T; 16] where T: Copy {
 fn main() -> io::Result<()> {
     init::initialize();
     let args: Vec<String> = env::args().collect();
-    // let input = Digest(vec_to_array(string_to_decimal(&args[1])));
-    let input = &args[1];
-    init::print_seq("🦝 Parsing md5 hash");
-    // let mut hashes: HashMap<Digest, String> = HashMap::new();
-    let mut hashes: HashMap<String, String> = HashMap::new();
     let rockyou = File::open("rockyou.txt")?;
     let reader = BufReader::new(rockyou);
     init::print_seq("🦝 Loading rockyou wordlist");
-    init::print_seq("🦝 Hashing wordlist with md5 algorithm");
-    for line in reader.lines() {
-        // let hash = md5(&line.as_ref().unwrap());
-        // hashes.insert(hash, line.unwrap());
-        let hash = sha256(&line.as_ref().unwrap());
-        // println!("Text: {:?}\n SHA256: {}", line.unwrap(), hash);
-        hashes.insert(hash, line.unwrap());
+    match args[1].as_str() {
+        "sha256" => sha256_search(&args[2], reader),
+        "md5" => md5_search(&args[2], reader),
+        _ => panic!("Error"),
     }
-    init::print_seq("🦝 Searching HashMap for password...");
-    match hashes.get(input) {
-        Some(value) => println!("🦝 Password found: <{}>", value.green().bold()),
-        None => println!("Password not found...")
-    }
+    
     init::print_seq("🦝 Exiting hashraccoon");
     Ok(())
 }
